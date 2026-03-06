18f9cab1bee46138a8fa436b20511c0deb49a0ed
Nandini Singh <singhnandini60810@gmail.com>
2026-03-06 13:35:51 +0530

    checkOnesSegment.py

---
+++ checkOnesSegment.py
@@ -0,0 +1,23 @@
+'''Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "1001"
+Output: false
+Explanation: The ones do not form a contiguous segment.
+Example 2:
+
+Input: s = "110"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s[i]​​​​ is either '0' or '1'.
+s[0] is '1'.'''
+class Solution:
+    def checkOnesSegment(self, s):
+        return "01" not in s
